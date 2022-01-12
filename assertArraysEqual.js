8c233372c19a03e4d3c21afbf8b5d8af994eb52c
chris <chris.bhall@outlook.com>
2022-01-12 11:28:45 +0000

    add assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    //console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  } else {
+    //console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let count = 0;
+  for (let i = 0; i < array1.length; i++) {
+    if (assertEqual(array1[i], array2[i])) {
+      count++;
+    }
+  }
+  if (count === array1.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)){
+    console.log("The arrays are equivalent");
+  } else {
+    console.log("The arrays are not equivalent");
+  }
+}

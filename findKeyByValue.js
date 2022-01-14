05e9af0e172b6d2751dafefdcac2db7a6409e000
chris <chris.bhall@outlook.com>
2022-01-14 15:11:53 +0000

    add fineKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+const findKeyByValue = function(obj, keyValue) {
+  const keys = Object.keys(obj);
+  for (const key in obj) {
+    if (keyValue === obj[key]) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
